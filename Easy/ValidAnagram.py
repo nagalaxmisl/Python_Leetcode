4957b746d15403dc74ff4be080ed4f69853db84e
NagalakshmiSL <nagalaxmislbhat@gmail.com>
2025-07-31 12:05:57 -0500

    Updated 242. Valid Anagram Solution

--- Easy/ValidAnagram.py
+++ Easy/ValidAnagram.py
@@ -5,11 +5,26 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+
+        return True
 
     def main(self):
         print(self.isAnagram(s = "anagram", t = "nagaram"))
         print(self.isAnagram(s = "rat", t = "car"))
+        print(self.isAnagram(s="rat", t="arr"))
 
 if __name__ == "__main__":
     sol = Solution()
