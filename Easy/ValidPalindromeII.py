7cec0b51317f8ea5503f425c25e09a1c2d842f4c
NagalakshmiSL <nagalaxmislbhat@gmail.com>
2025-09-12 11:02:35 -0500

    Solved Valid Palindrome

---
+++ Easy/ValidPalindromeII.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+    def main(self):
+        print(self.validPalindrome(s = "aba"))
+        print(self.validPalindrome(s = "abca"))
+        print(self.validPalindrome(s = "abc"))
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.main()
