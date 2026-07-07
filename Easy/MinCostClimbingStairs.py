d8b5a7a3c0c351927d24f140d537a38dbd545e15
NAGALAKSHMI\nagal <nagalaxmislbhat@gmail.com>
2026-07-07 12:10:47 -0500

    Solved Leetcode 746. Min Cost Climbing Stairs

---
+++ Easy/MinCostClimbingStairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(cost)
+
+        dp = [0] * n
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+
+        return min(dp[n-1], dp[n-2])
+
+sol = Solution()
+
+print(sol.minCostClimbingStairs([10,15,20]))
+print(sol.minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]))
